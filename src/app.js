41d625db52281cd76d47c194a6a80a52af90e14d
SlavaGomaniuk <110135497+SlavaGomaniuk@users.noreply.github.com>
2022-09-30 18:38:29 +0300

    add Last date update

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 let apiKey = "ed11d1043f1c6f1f2bbba5333d9da6cd";
 let city = "Kyiv";
-let now = new Date();
+
 let days = [
   "Sunday",
   "Monday",
@@ -10,33 +10,46 @@ let days = [
   "Friday",
   "Saturday",
 ];
-let day = days[now.getDay()];
-let minutes = `${now.getMinutes()}`.padStart(2, 0);
-let currentDate = `${day}, ${now.getHours()}:${minutes}`;
-let date = document.querySelector("#date");
-date.innerHTML = currentDate;
+
+//let currentDate = `${day}, ${now.getHours()}:${minutes}`;
+
+//date.innerHTML = currentDate;
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
-
+function formatDate(timestemp) {
+  let now = new Date(timestemp);
+  let day = days[now.getDay()];
+  let minutes = `${now.getMinutes()}`.padStart(2, 0);
+  return `${day}, ${now.getHours()}:${minutes}`;
+}
 function startCity(response) {
-  let city = "Kyiv";
   let cityStart = document.querySelector("#cityStart");
-  cityStart.innerHTML = `${city}`;
+
   console.log(response);
   let tempElement = document.querySelector("#startTemp");
-  tempElement.innerHTML = Math.round(response.data.main.temp);
-  let conditionNow = response.data.weather[0].main;
+  let conditionNow1 = response.data.weather[0].main;
+  let conditionNow2 = response.data.weather[0].description;
   let startCondition1 = document.querySelector("#condition1");
   let startCondition2 = document.querySelector("#condition2");
-  console.log(startCondition1);
-  let conditionNow2 = conditionNow.toLowerCase();
-  startCondition1.innerHTML = `${conditionNow}`;
-  startCondition2.innerHTML = `${conditionNow2}`;
   let windStart = response.data.wind.speed;
   let speed = document.querySelector("#wind");
-  speed.innerHTML = `${windStart} `;
   let humStart = response.data.main.humidity;
   let hum = document.querySelector("#humidity");
+  let icon = document.querySelector("#icon");
+  console.log(startCondition1);
+  let dateStart = document.querySelector("#date");
+  //let conditionNow2 = conditionNow.toLowerCase();
+  cityStart.innerHTML = `${city}`;
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  startCondition1.innerHTML = `${conditionNow1}`;
+  startCondition2.innerHTML = `${conditionNow2}`;
+  speed.innerHTML = `${windStart} `;
   hum.innerHTML = `${humStart}`;
+  icon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  icon.setAttribute("alt", `${response.data.weather[0].description}`);
+  dateStart.innerHTML = formatDate(response.data.dt * 1000);
 }
 axios.get(apiUrl).then(startCity);
