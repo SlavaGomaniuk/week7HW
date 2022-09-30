472d0ce6bcdad3496ec50dd796f67930b68ae7d6
SlavaGomaniuk <110135497+SlavaGomaniuk@users.noreply.github.com>
2022-09-30 20:02:29 +0300

    add fontawesom, function current position

--- src/app.js
+++ src/app.js
@@ -91,3 +91,51 @@ function startCity(response) {
 axios.get(apiUrl).then(startCity);
 let buttonSearch = document.querySelector("#search");
 buttonSearch.addEventListener("click", showCity);
+
+function currentPosition(position) {
+  console.log("currentPosition");
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(url2).then(function (response) {
+    let geoCity = response.data.name;
+    console.log(url2);
+    console.log(response);
+    let tempElement = document.querySelector("#startTemp");
+    let conditionNow1 = response.data.weather[0].main;
+    let conditionNow2 = response.data.weather[0].description;
+    let startCondition1 = document.querySelector("#condition1");
+    let startCondition2 = document.querySelector("#condition2");
+    let windStart = response.data.wind.speed;
+    let speed = document.querySelector("#wind");
+    let humStart = response.data.main.humidity;
+    let hum = document.querySelector("#humidity");
+    let icon = document.querySelector("#icon");
+    let dateStart = document.querySelector("#date");
+    //let conditionNow2 = conditionNow.toLowerCase();
+    cityStart.innerHTML = `${response.data.name} <i class="fa-solid fa-location-dot"></i>`;
+    tempElement.innerHTML = Math.round(response.data.main.temp);
+    startCondition1.innerHTML = `${conditionNow1}`;
+    startCondition2.innerHTML = `${conditionNow2}`;
+    speed.innerHTML = `${windStart} `;
+    hum.innerHTML = `${humStart}`;
+    icon.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    icon.setAttribute("alt", `${response.data.weather[0].description}`);
+    dateStart.innerHTML = formatDate(response.data.dt * 1000);
+  });
+}
+function geoLoc() {
+  console.log("geoLoc");
+  navigator.geolocation.getCurrentPosition(currentPosition);
+}
+function start(event) {
+  event.preventDefault();
+  console.log("start");
+  geoLoc();
+}
+let buttonCurrent = document.querySelector("#current");
+console.log(buttonCurrent);
+buttonCurrent.addEventListener("click", start);
